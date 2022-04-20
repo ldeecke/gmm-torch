ff9cb690429d396a2aeb61ff7e3e9921fdecc793
Miles Cranmer <miles.cranmer@gmail.com>
2022-04-20 00:19:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = ""
+
+
+setuptools.setup(
+    name="gmm_tools",
+    version="v0.0.1",
+    url="https://github.com/MilesCranmer/gmm-torch",
+    install_requires=["numpy", "torch", "scipy"],
+)
