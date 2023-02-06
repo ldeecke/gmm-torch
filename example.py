73851f2c1ae59bae2a24d6953f03cf758c8d53a9
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-06 08:46:33 -0800

    Means are collapsing

--- example.py
+++ example.py
@@ -3,9 +3,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="white", font="Arial")
-colors = sns.color_palette("Paired", n_colors=12).as_hex()
 
-import numpy as np
 import torch
 
 from gmm import GaussianMixture
@@ -33,36 +31,35 @@ def main():
     plot(data, y)
 
 
-def plot(data, y, iter):
-    n = y.shape[0]
+def plot(data, true_y, pred_y, iter, mus):
+    n = true_y.shape[0]
 
     fig, ax = plt.subplots(1, 1, figsize=(1.61803398875*4, 4))
     ax.set_facecolor("#bbbbbb")
     ax.set_xlabel("Dimension 1")
     ax.set_ylabel("Dimension 2")
 
-    # plot the locations of all data points ..
-    for i, point in enumerate(data.data):
-        if i <= n//2:
-            # .. separating them by ground truth ..
-            ax.scatter(*point, color="#000000", s=3, alpha=.75, zorder=n+i)
-        else:
-            ax.scatter(*point, color="#ffffff", s=3, alpha=.75, zorder=n+i)
+    colors = ['black', 'white', 'red']
 
-        if y[i] == 0:
-            # .. as well as their predicted class
-            ax.scatter(*point, zorder=i, color="#dbe9ff", alpha=.6, edgecolors=colors[1])
-        elif y[i] == 1:
-            ax.scatter(*point, zorder=i, color="#aaaaaa", alpha=.6, edgecolors=colors[2])
-        else:
-            ax.scatter(*point, zorder=i, color="#ffdbdb", alpha=.6, edgecolors=colors[5])
+    for point_idx, point in enumerate(data.data):
+        true_label = true_y[point_idx]
+        pred_label = pred_y[point_idx]
+        ax.scatter(*point, color=colors[true_label], s=3, alpha=0.75, zorder=n+point_idx)
+        ax.scatter(*point, color=colors[pred_label], s=50, edgecolors=colors[pred_label], alpha=0.6, zorder=point_idx)
 
-    handles = [plt.Line2D([0], [0], color="w", lw=4, label="Ground Truth 1"),
-        plt.Line2D([0], [0], color="black", lw=4, label="Ground Truth 2"),
-        plt.Line2D([0], [0], color=colors[1], lw=4, label="Predicted 1"),
-        plt.Line2D([0], [0], color=colors[5], lw=4, label="Predicted 2")]
+    for mu_idx, mu in enumerate(mus.data[0]):
+        print(mus)
+        print(mu)
+        ax.scatter(*mu, color=colors[mu_idx], marker='x', s=100, zorder=2*n)
 
-    legend = ax.legend(loc="best", handles=handles)
+    # handles = [
+    #     plt.Line2D([0], [0], color="white", lw=4, label="Ground Truth 1"),
+    #     plt.Line2D([0], [0], color="black", lw=4, label="Ground Truth 2"),
+    #     plt.Line2D([0], [0], color=colors[1], lw=4, label="Predicted 1"),
+    #     plt.Line2D([0], [0], color=colors[5], lw=4, label="Predicted 2"),
+    # ]
+    #
+    # legend = ax.legend(loc="best", handles=handles)
 
     plt.tight_layout()
     plt.savefig("example" + str(iter) + ".pdf")
